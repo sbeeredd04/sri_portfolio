32e6af14ecdb7414a82998dbc8fb1aae2460a72c
Sri <sbeeredd@asu.edu>
2023-08-01 22:53:23 +0530

    First page with all the deets

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Student", "Undergraduate Researcher", "Software Developer", "Web Developer", "Programmer", "Learner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
